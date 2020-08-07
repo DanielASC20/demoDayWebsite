4d4f2b05944524febd8a50c55bbbbd1d05e840ad
unknown <dtorres@gocfs.net>
2020-08-07 15:59:11 -0400

    added sidebar, beginning of index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementsByClassName("sidebar")[0].style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementsByClassName("sidebar")[0].style.width = "0";
+  }
